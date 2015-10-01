9a2741920732f6aaab776e3c1e19be74d4cfce99
Mohamed <moe.habib9@gmail.com>
2015-10-01 19:18:19 +0100

    Added scaleratio and outputsize parameters

--- util/serializedObj2binaryMask.m
+++ util/serializedObj2binaryMask.m
@@ -1,19 +1,33 @@
-function [ varargout ] = serializedObj2binaryMask( serializedObj )
+function [ varargout ] = serializedObj2binaryMask( serializedObj, varargin )
 %SERIALIZEDOBJ2BINARYMASK Converts a serialized object from the GUI to a
 %binary mask where the locations of the ROIs are 1 and the rest of the
 % image is zeros
 %  * serializedObj: The object that was stored by the GUI interface (usually
 %   loaded by the load function 'getSerializedObject')
 %  * labels: A
+% *scaleratio: parameter that controls how scaled the MA objects are
+% (default=1)
+% *outputsize: parameter that controls the output size of the resulting
+% image
+
     categories = serializedObj.categories;
-    size = serializedObj.imageSize;
-    resmask = zeros( [size(1), size(2)] ) ;
+    imsize = serializedObj.imageSize;
+
+    
+    p = inputParser();
+    addParameter(p, 'scaleratio',1);
+    addParameter(p, 'outputsize', imsize);
+    parse(p, varargin{:});
+    scaleratio = p.Results.scaleratio;
+    outputsize = p.Results.outputsize;
+    
+    resmask = zeros( [outputsize(1), outputsize(2)] ) ;
     for i=1:length(categories)
         label = categories(i).label;
         shape = categories(i).shape;
         data = serializedObj.(label).data;
         
-        [mask, cellarr] = dataExtract( data, shape, size );
+        [mask, cellarr] = dataExtract( data, shape, outputsize, scaleratio );
         resmask(mask) = i;
         
     end
@@ -27,22 +41,22 @@ function [ varargout ] = serializedObj2binaryMask( serializedObj )
     end
         
 
-    function [mask, cellarray] = dataExtract(data, shape, imsize)
+    function [mask, cellarray] = dataExtract(data, shape, outsize, scaleratio)
         
         % Create an invisible figure to draw shapes and get corresponding
         % masks
         hndl = figure('visible', 'off');
         
-        resMask = false( [imsize(1), imsize(2)] );
+        resMask = false( [outsize(1), outsize(2)] );
         resCellArray = {};
         
         for i=1:length(data)
             
             % Display an empty image on the hidden figure
-            tmp = false([imsize(1), imsize(2)]);
+            tmp = false([outsize(1), outsize(2)]);
             im_hndl = imshow(tmp);
             
-            position = data{i};
+            position = data{i}*scaleratio;
             
             % Draw the shape on the figure
             shp = drawShape(hndl, shape, position);
