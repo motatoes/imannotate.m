721f5c19f0f085b7d392df35adc60409fb540489
Mohamed <moe.habib9@gmail.com>
2016-08-02 12:10:35 +0100

    renamed gui file

--- imannotate.m
+++ imannotate.m
@@ -1,35 +1,38 @@
-function varargout = main(varargin)
-% MAIN MATLAB code for main.fig
-%      MAIN, by itself, creates a new MAIN or raises the existing
+function varargout = imannotate(varargin)
+
+import imannotate.*;
+import imannotate.util.*;
+
+% IMANNOTATE MATLAB code for imannotate.fig
+%      IMANNOTATE, by itself, creates a new IMANNOTATE or raises the existing
 %      singleton*.
 %
-%      H = MAIN returns the handle to a new MAIN or the handle to
+%      H = IMANNOTATE returns the handle to a new IMANNOTATE or the handle to
 %      the existing singleton*.
 %
-%      MAIN('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in MAIN.M with the given input arguments.
+%      IMANNOTATE('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in IMANNOTATE.M with the given input arguments.
 %
-%      MAIN('Property','Value',...) creates a new MAIN or raises the
+%      IMANNOTATE('Property','Value',...) creates a new IMANNOTATE or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before main_OpeningFcn gets called.  An
+%      applied to the GUI before imannotate_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to main_OpeningFcn via varargin.
+%      stop.  All inputs are passed to imannotate_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help main
+% Edit the above text to modify the response to help imannotate
 
-% Last Modified by GUIDE v2.5 02-Dec-2015 12:05:24
-
+% Last Modified by GUIDE v2.5 28-Feb-2016 03:41:42
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @main_OpeningFcn, ...
-                   'gui_OutputFcn',  @main_OutputFcn, ...
+                   'gui_OpeningFcn', @imannotate_OpeningFcn, ...
+                   'gui_OutputFcn',  @imannotate_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -44,24 +47,21 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before main is made visible.
-function main_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before imannotate is made visible.
+function imannotate_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to main (see VARARGIN)
+% varargin   command line arguments to imannotate (see VARARGIN)
+    
 
-
-% Add the utility functions path
-addpath util
-
-% Choose default command line output for main
+% Choose default command line output for imannotate
 handles.output = hObject;
 
 
 % Loading some variables from the settings file (SETTINGS struct)
-settings
+SETTINGS = imannotate.Imannotate_Settings();
 %===========================
 
 handles.SETTINGS = SETTINGS;
@@ -73,10 +73,10 @@ handles.imageViewer = figure();
 
 
 
-% UIWAIT makes main wait for user response (see UIRESUME)
+% UIWAIT makes imannotate wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
-handles.markers = MarkerCollection(handles.SETTINGS.CATEGORIES, handles.imageViewer);
+handles.markers = imannotate.MarkerCollection(handles.SETTINGS.CATEGORIES, handles.imageViewer);
 
 for i=1:length(SETTINGS.CATEGORIES)
     category = SETTINGS.CATEGORIES(i).label;
@@ -92,7 +92,7 @@ end
 guidata(hObject, handles);
 
 % --- Outputs from this function are returned to the command line.
-function varargout = main_OutputFcn(hObject, eventdata, handles) 
+function varargout = imannotate_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -129,7 +129,7 @@ filename = strcat(handles.SETTINGS.MARKERS_PATH, '\', items{index_selected}, mar
 handles.markers.reset();
 
 if exist(filename, 'file')
-    markerPositions = getSerializedObject( filename  );
+    markerPositions = imannotate.util.getSerializedObject( filename  );
 %     load(filename);
     
     handles.markers.loadMarkers(markerPositions);
@@ -233,7 +233,7 @@ function visualizeBtn_Callback(hObject, eventdata, handles)
 % hObject    handle to visualizeBtn (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-addpath util;
+
 img = handles.currentImage;
 res = img;
 markerPositions = handles.markers.serialize();
@@ -249,3 +249,13 @@ figure; imshow(res);
 function addMarker(hObject, eventdata, handles, category)
     handles.markers.addMarker(category);
 %=================== User Funcations ==============================%
+
+
+% --- Executes on key press with focus on figure1 and none of its controls.
+function figure1_KeyPressFcn(hObject, eventdata, handles)
+% hObject    handle to figure1 (see GCBO)
+% eventdata  structure with the following fields (see MATLAB.UI.FIGURE)
+%	Key: name of the key that was pressed, in lower case
+%	Character: character interpretation of the key(s) that was pressed
+%	Modifier: name(s) of the modifier key(s) (i.e., control, shift) pressed
+% handles    structure with handles and user data (see GUIDATA)
